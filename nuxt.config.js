8f77c6018050c86de7fba8cd79d84a6f3f9c152d
Konstantin BIFERT <konstantin.bifert@gmail.com>
2022-05-16 18:45:40 +0200

    done

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,44 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt2-example-i18n',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  buildModules: ['@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    defaultLocale: 'en',
+    langDir: 'locales/',
+    lazy: true,
+    loadLanguagesAsync: true,
+    locales: [
+      {
+        code: 'en',
+        iso: 'en',
+        file: 'en.json',
+      },
+      {
+        code: 'fr',
+        iso: 'fr',
+        file: 'fr.json',
+      },
+    ],
+    vueI18n: {
+      fallbackLocale: 'en',
+    },
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <Tutorial/>
+  <div>
+    <nuxt-link :to="switchLocalePath('en')">Change to English</nuxt-link>
+    <nuxt-link :to="switchLocalePath('fr')">Passer en Francais</nuxt-link>
+    <p>here is the i18n string {{ $t('helloWorld') }}</p>
+  </div>
 </template>
-
-<script>
-export default {}
-</script>
